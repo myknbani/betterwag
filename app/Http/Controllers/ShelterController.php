c7ad67d217f5963f26b70c03ee5709f03953681c
Richard Michael Coo <richard.michael.coo@gmail.com>
2026-06-18 01:47:47 +0800

    fix: fix linter errors

--- app/Http/Controllers/ShelterController.php
+++ app/Http/Controllers/ShelterController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CreateShelterRequest;
 use App\Http\Requests\UpdateShelterRequest;
 use App\Models\Shelter;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class ShelterController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
         $shelters = Shelter::orderBy('name', 'asc')->get();
 
@@ -21,17 +23,17 @@ class ShelterController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(CreateShelterRequest $request)
+    public function store(CreateShelterRequest $request): JsonResponse
     {
         $shelter = Shelter::create($request->validated());
 
-        return response()->json($shelter, 201);
+        return response()->json($shelter, Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Shelter $shelter)
+    public function show(Shelter $shelter): JsonResponse
     {
         return response()->json($shelter);
     }
@@ -39,17 +41,17 @@ class ShelterController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateShelterRequest $request, Shelter $shelter)
+    public function update(UpdateShelterRequest $request, Shelter $shelter): JsonResponse
     {
         $shelter->update($request->validated());
 
-        return response()->json($shelter);
+        return response()->json($shelter->refresh());
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Shelter $shelter)
+    public function destroy(Shelter $shelter): Response
     {
         $shelter->delete();
 
