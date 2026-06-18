58760f419f2242e33c841f92c2119f542bf5f856
Richard Michael Coo <richard.michael.coo@gmail.com>
2026-06-18 18:38:08 +0800

    fix: make read endpoints public

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,13 @@ use App\Http\Controllers\ShelterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::apiResource('shelters', ShelterController::class)->only(['index', 'show']);
+Route::apiResource('shelters.dogs', DogController::class)->only(['index', 'show'])->shallow();
+
 Route::middleware('auth:sanctum')
     ->group(function () {
         Route::get('/user', fn (Request $request) => $request->user());
 
-        Route::apiResource('shelters', ShelterController::class);
-        Route::apiResource('shelters.dogs', DogController::class)->shallow();
+        Route::apiResource('shelters', ShelterController::class)->except(['index', 'show']);
+        Route::apiResource('shelters.dogs', DogController::class)->shallow()->except(['index', 'show']);
     });
