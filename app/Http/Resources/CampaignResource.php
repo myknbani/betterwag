6a553359c5b309b0dab96cb22755471b8a88ea51
Richard Michael Coo <richard.michael.coo@gmail.com>
2026-06-19 00:21:08 +0800

    feat: add campaign resource

---
+++ app/Http/Resources/CampaignResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CampaignResource extends JsonResource
+{
+    /** @return array<string, mixed> */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'shelterId' => $this->shelter_id,
+            'dogId' => $this->dog_id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'type' => $this->type,
+            'status' => $this->status,
+            'goalAmount' => $this->goal_amount,
+            'closedAt' => $this->closed_at,
+            'closedReason' => $this->closed_reason,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+        ];
+    }
+}
